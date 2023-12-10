761938ba8dd2771b3f5df3fd406807c359c2c22c
purge710 <johnrussel338@gmail.com>
2023-12-10 16:29:44 +0800

    add

--- lashopee-frontend/src/App.js
+++ lashopee-frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Views from './views';
-import Cart from './views/Cart';
-import CategoryDetail from './views/Category';
-import ProductDetail from './views/Product';
-import NotFound from './views/NotFound';
+
 import { Provider } from 'react-redux';
 import store from './redux/store/Index';
-import Header from './components/shared/Header';
-import Landing from './views/Landing';
 
 
 
